da2b2a891c3067a1dab961e8178fb15c0b18ceb2
Edisonfb <edison.fbnts@gmail.com>
2020-07-08 21:37:48 -0300

    blablabla

---
+++ Assets/Scripts/Key.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Key : MonoBehaviour
+{
+    public GameObject theDoor;
+
+    public void Restart()
+    {
+        theDoor.SetActive(true);
+        gameObject.SetActive(true);
+    }
+
+    public void OpenDoor()
+    {
+        theDoor.SetActive(false);
+        gameObject.SetActive(false);
+    }
+}
